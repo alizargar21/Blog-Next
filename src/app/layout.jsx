06059022e29c78e32aa1364fe8722e3aeac19146
Ali Zargar <mrzargar1995@gmail.com>
2024-10-19 16:55:08 +0330

    add ui components and icon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,7 @@
 
+import vazirFont from "@/constants/localFont";
 import "../styles/globals.css";
+import Header from "@/components/Header";
 
 
 export const metadata = {
@@ -10,7 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
-      <body>{children}</body>
+      <body className={`${vazirFont.variable} font-sans`}>
+        <Header />
+        <div className="container">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
